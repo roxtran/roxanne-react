10116a1efa24dab027008766e993af850af1393e
Roxanne <tran.giadung@gmail.com>
2021-04-13 09:16:13 -0400

    update text

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,8 @@ const About = () => {
     <StyledAbout id='about'>
       <h2>About Me</h2>
       <p>
-        My name is Roxanne Tran. I am a full stack designer and consultant with
-        over 2+ years of experience, currently, working remotely for clients
-        around the world.
+        My name is Roxanne Tran. I am a full stack designer and developer,
+        currently, working remotely for clients around the world.
         <br />
         <br />I focus on working with growing startups to increase their results
         by either working with their brand/product directly or via consulting
