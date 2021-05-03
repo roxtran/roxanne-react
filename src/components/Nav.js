84aba579b59da2cb3ff55fa83c1189533d724a46
Roxanne <tran.rox@gmail.com>
2021-05-03 14:30:29 -0400

    update About Me position

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -37,7 +37,7 @@ const Nav = () => {
             duration={500}
             spy={true}
             exact='true'
-            offset={-55}
+            offset={-125}
           >
             About Me
           </Scroll>
