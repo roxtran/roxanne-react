f061da3cc49bae9196b3436de36f3b9b6edfca49
Roxanne Tran <tran.rox@gmail.com>
2021-07-10 23:24:45 -0400

    .

--- src/App.js
+++ src/App.js
@@ -2,62 +2,39 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
-// import About from './components/About'
-// import Contact from './components/Contact'
+import About from './components/About'
+import Contact from './components/Contact'
 
 // import styles
 import './styles/_variables.css'
-// import GlobalStyle from './styles/GlobalStyle'
+import GlobalStyle, { FooterBG } from './styles/GlobalStyle'
 
 // Animation
-// import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 
 // Lazy loading
 import { lazy, Suspense } from 'react'
 const Projects = lazy(() => import('./components/Projects'))
-const About = lazy(() => import('./components/About'))
-const Contact = lazy(() => import('./components/Contact'))
-const GlobalStyle = lazy(() => import('./styles/GlobalStyle'))
-const AnimatePresence = lazy(() =>
-  import('framer-motion').then((module) => ({
-    default: module.AnimatePresence,
-  }))
-)
-const FooterBG = lazy(() =>
-  import('./styles/GlobalStyle').then((module) => ({
-    default: module.FooterBG,
-  }))
-)
 
 function App() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <AnimatePresence>
-        <Suspense fallback={<div>Loading...</div>}>
-          <GlobalStyle />
-        </Suspense>
-        <div className='container'>
-          <Nav />
-          <main>
-            <Hero />
-            <Skills />
-            {/* <Projects /> */}
-            <Suspense fallback={<div>Loading...</div>}>
-              <Projects />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-              <About />
-            </Suspense>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Contact />
-            </Suspense>
-          </main>
-        </div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <FooterBG />
-        </Suspense>
-      </AnimatePresence>
-    </Suspense>
+    <AnimatePresence>
+      <GlobalStyle />
+      <div className='container'>
+        <Nav />
+        <main>
+          <Hero />
+          <Skills />
+          {/* <Projects /> */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Projects />
+          </Suspense>
+          <About />
+          <Contact />
+        </main>
+      </div>
+      <FooterBG />
+    </AnimatePresence>
   )
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import circle1w from '../img/circle1.webp'
 import circle1 from '../img/circle1a.png'
@@ -16,13 +16,11 @@ const Hero = () => {
       exit='exit'
     >
       <HeroHeader className='hide-overflow'>
-        <Suspense fallback={<div>Loading...</div>}>
-          <motion.picture variants={circleAnim}>
-            <source srcSet={circle1w} type='image/webp' />
-            <source srcSet={circle1} type='image/jpeg' />
-            <img src={circle1} alt='circle1' />
-          </motion.picture>
-        </Suspense>
+        <motion.picture variants={circleAnim}>
+          <source srcSet={circle1w} type='image/webp' />
+          <source srcSet={circle1} type='image/jpeg' />
+          <img src={circle1} alt='circle1' />
+        </motion.picture>
         {/* <motion.img variants={circleAnim} src={circle1} alt='circle1' /> */}
         <motion.h3 variants={slideUp}>
           Roxanne<span className='highlight'>.</span>
