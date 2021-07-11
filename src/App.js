a6c9b81efdcdd3b19863723ac825d16836a8b569
Roxanne <tran.rox@gmail.com>
2021-07-10 21:37:19 -0400

    added code splitting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 // import components
 import Nav from './components/Nav'
 import Hero from './components/Hero'
-import Skills from './components/Skills'
-import About from './components/About'
-import Contact from './components/Contact'
-// import Projects from './components/Projects'
-
 // import styles
 import './styles/_variables.css'
 import GlobalStyle, { FooterBG } from './styles/GlobalStyle'
-
 // Animation
 import { AnimatePresence } from 'framer-motion'
-
-// Lazy loading
+// import Lazy loading
 import { lazy, Suspense } from 'react'
+const Skills = lazy(() => import('./components/Skills'))
 const Projects = lazy(() => import('./components/Projects'))
+const About = lazy(() => import('./components/About'))
+const Contact = lazy(() => import('./components/Contact'))
 
 function App() {
   return (
@@ -25,13 +21,19 @@ function App() {
         <Nav />
         <main>
           <Hero />
-          <Skills />
           {/* <Projects /> */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Skills />
+          </Suspense>
           <Suspense fallback={<div>Loading...</div>}>
             <Projects />
           </Suspense>
-          <About />
-          <Contact />
+          <Suspense fallback={<div>Loading...</div>}>
+            <About />
+          </Suspense>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Contact />
+          </Suspense>
         </main>
       </div>
       <FooterBG />
